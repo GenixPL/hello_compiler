3a7803729fcef231729fab434f7ca2d67b0535e5
GenixPL <intergenixyt@gmail.com>
2026-09-26 11:44:51 +0200

    move to folder

--- scripts/compile_n_run.sh
+++ scripts/compile_n_run.sh
@@ -1,3 +1,3 @@
-mkdir -p build/ &&
-clang lib/*.s -o build/hello &&
-./build/hello
+mkdir -p test_assembly/build/ &&
+clang test_assembly/lib/*.s -o test_assembly/build/hello &&
+./test_assembly/build/hello
